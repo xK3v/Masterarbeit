381b8ef4811e84de22ec59dc770db5b84e35c92a
Kevin <kazianschuetz.kevin@gmail.com>
2022-10-31 20:49:26 +0100

    add files

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# To enable ssh & remote debugging on app service change the base image to the one below
+# FROM mcr.microsoft.com/azure-functions/python:4-python3.9-appservice
+FROM mcr.microsoft.com/azure-functions/python:4-python3.9
+
+RUN apt-get install -y gcc git unzip python3-dev libgl1-mesa-glx # python3-devel.x86_64 mesa-libGL
+
+ENV AzureWebJobsScriptRoot=/home/site/wwwroot \
+    AzureFunctionsJobHost__Logging__Console__IsEnabled=true
+
+RUN pip install Cython==0.29.30
+RUN pip install numpy==1.22.4
+
+COPY requirements.txt /
+#RUN pip install -r /requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY . /home/site/wwwroot
---
+++ PyOdmStart/__init__.py
@@ -0,0 +1,37 @@
+import logging
+import os
+from pyodm import Node
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+    
+    dir = '/datashare/Fotos_3/W'
+    files = os.listdir(dir)
+    files = [os.path.join(dir, f) for f in files]
+
+    n = Node('nodeodm-sunshaine.azurewebsites.net', 80)
+    task = n.create_task(files[:6], name=name)
+    #task.wait_for_completion()
+    #task.download_results()
+
+    if name:
+        #return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        #return func.HttpResponse("Task created with name: %s" % task.info.name)
+        return func.HttpResponse("Success")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
