80c60a2889b87807f79e843b492a63c1616ffede
xK3v <kazianschuetz.kevin@gmail.com>
2022-11-04 14:53:14 +0100

    remove odm settings

--- PyOdmStart/__init__.py
+++ PyOdmStart/__init__.py
@@ -22,23 +22,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     files = [os.path.join(dir, f) for f in files]
 
     n = Node('nodeodm-sunshaine.azurewebsites.net', 80)
-    task = n.create_task(files, name=name, options=
-        {
-            "auto-boundary": True,
-            "dem-resolution": 3,
-            "depthmap_resolution": 1280,
-            "dsm": True,
-            "feature-quality": "ultra",
-            "gps-accuracy": 3,
-            "mesh-octree-depth": 12,
-            "mesh-size": 400000,
-            "min-num-features": 15000,
-            "orthophoto-cutline": True,
-            "orthophoto-resolution": 3,
-            "pc-quality": "ultra",
-            "resize-to": -1,
-            "skip-3dmodel": True
-        })
+
+    task = n.create_task(files, name=name)
+
     #task.wait_for_completion()
     #task.download_results()
 
