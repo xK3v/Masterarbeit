59fa9f2fcd725953689780f1450a9ce3c7be30e8
xK3v <kazianschuetz.kevin@gmail.com>
2022-11-11 19:08:07 +0100

    trying out odm options

--- PyOdmStart/__init__.py
+++ PyOdmStart/__init__.py
@@ -17,14 +17,35 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
     
-    dir = '/datashare/Fotos_3/W'
+    dir = '/datashare/Fotos_3/T'
     files = os.listdir(dir)
     files = [os.path.join(dir, f) for f in files]
 
     logging.info("Files loaded: %s" % len(files))
+
     n = Node('nodeodm-sunshaine.azurewebsites.net', 80)
 
-    task = n.create_task(files, name=name)
+    task = n.create_task(files, options = {
+        'dsm' : True,
+        'orthophoto-resolution' : 3,
+        'skip-3dmodel' : True,
+        'force-gps' : True,
+        'ignore-gsd' : True,
+        'resize-to' : -1,
+
+        #'pc-csv' : True,
+        #'min-num-features' : 15000,
+        'orthophoto-cutline' : True,
+        #'pc-quality' : 'ultra',
+        #'mesh-size' : 400000,
+        #'texturing-skip-global-seam-leveling' : True,
+        #'feature-quality' : 'ultra',
+        #'depthmap-resolution' : 1280,
+        #'dem-resolution' : 3,
+        #'gps-accuracy' : 3,
+        #'mesh-octree-depth' : 12,
+        #'auto-boundary' : True
+    }, name=name)
 
     #task.wait_for_completion()
     #task.download_results()
